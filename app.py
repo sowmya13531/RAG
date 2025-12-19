255951df073b161fb90f6505a7d2d922151f2326
Sowmya Kanithi <Sowmya135@users.noreply.huggingface.co>
2025-12-19 07:16:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py
 import gradio as gr
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
-from langchain_text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
